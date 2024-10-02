cc2fc425070b60fc31b6960d36f1575e0cf64df2
sebitazz321 <167590994+sebitazz321@users.noreply.github.com>
2024-10-01 21:38:21 -0500

    Add files via upload

---
+++ SEBASTIAN/adelanto.js
@@ -0,0 +1,65 @@
+// PRINCIPALES FUNCIONES MATEMATICAS INTERNAS DE JAVA SCRIPT.
+var precio = Math.ceil(4359.9);
+document.write('El precio redondeado:', precio);
+document.write ('<br>');
+// Precio redondeado
+var precio1 = Math.floor(540.2);
+document.write ('El precio redondeado de este producto es:', precio1);
+document.write('<br>');
+// Precio redondeado
+var precio2 = Math.round(599.45);
+document.write ('El precio de este articulo redondedo es:', precio2);
+document.write ('<br>');
+// Seno de un angulo
+var seno = Math.sin (45);
+document.write ('El valor del angulo 45 es:', seno);
+document.write ('<br>');
+// Coseno de un angulo
+var coseno = Math.cos (38);
+document.write ('El coseno del angulo de 38 es:', coseno);
+document.write('<br>');
+// Tangente de un angulo
+var tangente = Math.tan (38);
+document.write ('La tangente del angulo de 38 es:', tangente);
+document.write('<br>');
+//Exponencial de un numero
+var exponencial = Math.exp (7);
+document.write ('Expnencial de 7 es:', exponencial);
+document.write ('<br>');
+// Logaritmo de un numeго
+var logaritmo = Math.log (75);
+document.write ('El logaritmo de 75 es:', logaritmo);
+document.write ('<br>');
+//Valor absoluto
+var absoluto = Math.abs (-85);
+document.write ('El valor absoluto de 85 es:', absoluto);
+document.write ('<br>');
+// Numero maximo
+var maximo = Math.max (7, 36, 99, 294);
+document.write ('El valor mayor es:', maximo);
+document.write ("<br>");
+// Numero minimo
+var minimo = Math.min (15, 36, 99, 294);
+document.write ('El valor minimo es: ',minimo);
+document.write ('<br>');
+// Nuemero aleatorio en decimal
+var aleatorio = Math.random();
+document.write ('El valor aleatorio: ',aleatorio);
+document.write('<br>');
+// Numero aleatorio del 1 al 10 en decinales
+// Nuemero aleatorio en decimal
+var aleatorio1 = Math.random()*10;
+document.write ('El valor aleatorio: ',aleatorio1);
+document.write('<br>');
+//Numero aleatorio redondeado
+var aleatorio2 = Math.round (Math.random()*8);
+document.write ('El valor aleatorio: ', aleatorio2);
+document.write ('<br>');
+//Raiz cuadrada de un numero
+var valor = Math.sqrt(49);
+document.write('La raiz cuadrada de 49 es: ', valor);
+document.write('<br>');
+// calcular exponente de un mumero
+var exponente = Math.pow(4,3);
+document.write('El valor de 4 elevado a la 2 es: ', exponente);
+document.write('<br>');
---
+++ SEBASTIAN/operadores.js
@@ -0,0 +1,14 @@
+let a = 6 ;
+
+
+document.write ('el valor de a: ' ,a);
+document.write ('<br>');
+a++;
+document.write ('el valor de a: ' ,a );
+document.write ('<br>');
+a -= 2;
+document.write ('el valor de a: ' ,a );
+document.write ('<br>');
+a *= 5;
+document.write ('el valor de a: ' ,a );
+document.write ('<br>');
